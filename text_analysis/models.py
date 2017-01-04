4e182f4b07476a282dd3d42749fdc57ead0079e2
Lei <zhenglei.cs@gmail.com>
2017-01-04 11:19:12 -0600

    update to new keras version

--- text_analysis/models.py
+++ text_analysis/models.py
@@ -7,11 +7,10 @@ import numpy as np
 np.random.seed(1337)
 
 from keras.callbacks import EarlyStopping
-from keras.layers.containers import Sequential
-from keras.layers.convolutional import Convolution2D, MaxPooling2D
-from keras.layers.core import Reshape, Flatten, Dropout, Dense
+from keras.models import *
+from keras.layers.convolutional import *
+from keras.layers.core import *
 from keras.layers.embeddings import Embedding
-from keras.models import Graph
 from keras.preprocessing import sequence
 
 
@@ -30,11 +29,12 @@ class CNN_module():
         vanila_dimension = 200
         projection_dimension = output_dimesion
 
+        '''
         filter_lengths = [3, 4, 5]
         self.model = Graph()
 
-        '''Embedding Layer'''
-        self.model.add_input(name='input', input_shape=(max_len,), dtype=int)
+
+        self.model.add_input(name='input', input_shape=(max_len,), dtype='int')
 
         if init_W is None:
             self.model.add_node(Embedding(
@@ -43,7 +43,7 @@ class CNN_module():
             self.model.add_node(Embedding(max_features, emb_dim, input_length=max_len, weights=[
                                 init_W / 20]), name='sentence_embeddings', input='input')
 
-        '''Convolution Layer & Max Pooling Layer'''
+
         for i in filter_lengths:
             model_internal = Sequential()
             model_internal.add(
@@ -57,18 +57,39 @@ class CNN_module():
             self.model.add_node(model_internal, name='unit_' +
                                 str(i), input='sentence_embeddings')
 
-        '''Dropout Layer'''
+
         self.model.add_node(Dense(vanila_dimension, activation='tanh'),
                             name='fully_connect', inputs=['unit_' + str(i) for i in filter_lengths])
         self.model.add_node(Dropout(dropout_rate),
                             name='dropout', input='fully_connect')
-        '''Projection Layer & Output Layer'''
+
         self.model.add_node(Dense(projection_dimension, activation='tanh'),
                             name='projection', input='dropout')
 
         # Output Layer
         self.model.add_output(name='output', input='projection')
-        self.model.compile('rmsprop', {'output': 'mse'})
+        '''
+        self.model = Sequential()  # or Graph or whatever
+        self.model.add(Embedding(output_dim=emb_dim, input_dim=vocab_size,
+                                        mask_zero=False))
+        # user_review_model.add(Reshape((1,word_dim,seq_len)))
+        self.model.add(Convolution1D(nb_filter=64,
+                                filter_length=3,
+                                border_mode='valid',
+                                activation='relu',
+                                subsample_length=1))  # we use max over time pooling by defining a python function to use
+
+        # in a Lambda layer
+        def max_1d(X):
+            return K.max(X, axis=1)
+
+        self.model.add(Lambda(max_1d, output_shape=(64,)))
+
+        self.model.add(Dense(output_dimesion, activation='relu',name='output'))
+        #self.model.add(Dropout(0.2))
+
+        #self.model.compile('rmsprop', {'output': 'mse'})
+        self.model.compile(loss='mse',optimizer='rmsprop')
 
     def load_model(self, model_path):
         self.model.load_weights(model_path)
@@ -118,8 +139,9 @@ class CNN_module():
         V = np.random.permutation(V)
 
         print("Train...CNN module")
-        history = self.model.fit({'input': X_train, 'output': V},
-                                 verbose=0, batch_size=self.batch_size, nb_epoch=self.nb_epoch, shuffle=True, validation_split=0.1, callbacks=[EarlyStopping(monitor='val_loss', patience=0)])
+        #history = self.model.fit({'input': X_train, 'output': V},
+        #                         verbose=0, batch_size=self.batch_size, nb_epoch=self.nb_epoch, shuffle=True, validation_split=0.1, callbacks=[EarlyStopping(monitor='val_loss', patience=0)])
+        history = self.model.fit(X_train,y=V,batch_size=self.batch_size,nb_epoch=self.nb_epoch, shuffle=True, validation_split=0.1, callbacks=[EarlyStopping(monitor='val_loss', patience=0)])
 
         cnn_loss_his = history.history['loss']
         cmp_cnn_loss = sorted(cnn_loss_his)[::-1]
@@ -129,6 +151,10 @@ class CNN_module():
 
     def get_projection_layer(self, X_train):
         X_train = sequence.pad_sequences(X_train, maxlen=self.max_len)
-        Y = self.model.predict(
-            {'input': X_train}, batch_size=len(X_train))['output']
+        #Y = self.model.predict(
+        #    {'input': X_train}, batch_size=len(X_train))['output']
+
+        intermediate_layer_model = Model(input=self.model.input,
+                                         output=self.model.get_layer('output').output)
+        Y = intermediate_layer_model.predict(X_train,batch_size=len(X_train))
         return Y
